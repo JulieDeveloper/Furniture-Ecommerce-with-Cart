715541ac844169bdca3169720e1190352e53052d
Julie Chou <hi@juliechou.com>
2025-10-02 10:19:50 -0400

    feat: dynamically render product list

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ fetch('https://furniture-api.fly.dev/v1/products', options)
     .then(res => {
         rawProduct = res.data
         console.log(rawProduct);
+        productListEle.innerHTML = rawProduct.map(product => `
+            <div class="product-card">
+                <img src="${product.image_path}" alt="${product.name}" class="product-image"/>
+                <h2 class="product-name">${product.name}</h2>
+                <p class="product-price">$${product.price.toFixed(2)}</p>
+                <p class="product-description">${product.description}</p>
+            </div>
+        `).join('');
     })
 
     // handle error:
