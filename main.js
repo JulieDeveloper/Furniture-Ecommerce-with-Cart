4e0db8830922085fc44791079f88acd5c1978a9a
Julie Chou <chouyuan424@gmail.com>
2025-10-08 16:55:44 -0400

    fix: update product list html structure in js for rendering

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/11.6.1' } };
 let rawProduct = {};
-const productListEle = document.getElementById('product-list');
+
+// HTML elements: 
+const productsList_HTML = document.getElementById('product-grid');
+
+
+
+const endPoint = 'https://furniture-api.fly.dev/v1/products';
 
 // Fetch Product List
-fetch('https://furniture-api.fly.dev/v1/products', options)
+fetch(endPoint, options)
     .then(res => res.json())
     // handle success: 
     .then(res => {
         rawProduct = res.data
         console.log(rawProduct);
-        productListEle.innerHTML = rawProduct.map(product => `
-            <div class="product-card">
-                <img src="${product.image_path}" alt="${product.name}" class="product-image"/>
-                <h2 class="product-name">${product.name}</h2>
-                <p class="product-price">$${product.price.toFixed(2)}</p>
-                <p class="product-description">${product.description}</p>
+        productsList_HTML.innerHTML = rawProduct.map(product => `
+        <div class="product-card" data-id="${product.id}">
+            <img src="${product.image_path}" alt="${product.name}" class="product-image" alt="Product Image" />
+            <div class="product-details">
+                <div>
+                    <h2 class="product-name">${product.name}</h2>
+                    <p class="product-price">$${product.price.toFixed(2)}</p>
+                </div>
+                <div class="add-to-cart-btn">+</div>
             </div>
+        </div>
         `).join('');
     })
 
